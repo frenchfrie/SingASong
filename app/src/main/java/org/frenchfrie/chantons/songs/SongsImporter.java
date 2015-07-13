ef975b24061736e5902e63031927c9ee99218167
henri <hmbenque@workit.fr>
2015-07-13 18:37:32 +0200

    n,;bnn,;

--- app/src/main/java/org/frenchfrie/chantons/songs/SongsImporter.java
+++ app/src/main/java/org/frenchfrie/chantons/songs/SongsImporter.java
@@ -2,7 +2,9 @@ package org.frenchfrie.chantons.songs;
 
 import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
 
+import com.google.common.io.Files;
 import com.google.gson.Gson;
 
 import java.io.File;
@@ -10,9 +12,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.List;
 
-import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Lists.transform;
 
 public class SongsImporter {
 
@@ -42,8 +45,15 @@ public class SongsImporter {
             }
             Gson gson = new Gson();
             SongToSongDTO converter = new SongToSongDTO();
-            Iterable<SongDTO> songsDtos = transform(songsDAO.findAll(), converter);
-            gson.toJson(songsDtos, new FileWriter(songFile));
+            List<SongDTO> songsDtos = transform(songsDAO.findAll(), converter);
+            try (FileWriter fileWriter = new FileWriter(songFile)) {
+                String songsAsJson = gson.toJson(songsDtos, List.class);
+                fileWriter.append(songsAsJson);
+//                gson.toJson(songsDtos, fileWriter);
+                fileWriter.flush();
+            }
+            String storedSongs = Files.toString(songFile, Charset.defaultCharset());
+            Log.d(SongsImporter.class.getName(), storedSongs);
         } catch (IOException e) {
             e.printStackTrace();
         }
